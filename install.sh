496f388b69ebb69d0b4a7815a38ccd2225e59164
wyao <289672494@qq.com>
2019-06-18 14:45:02 +0800

    修改install.sh 安装sql

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 # 安装数据库
-mysql –u root –p wyao161211 -D revervation < views/controller/service/models/db/sql/create_db.sql
+mysql -uroot -pwyao161211 -D reservation < views/controller/service/models/db/sql/create_db.sql
