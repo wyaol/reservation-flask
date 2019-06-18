69d9645c177c21cc7def901f1eed0b38782b89db
wyao <289672494@qq.com>
2019-06-18 17:16:43 +0800

    纠正sql语句

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ chmod 777 operate.sh
 
 #### 安装pymysql操作数据库
 python3 -m pip install pymysql
+
+#### 安装数据库
+msyql 创建数据库 reservation
+chmod 777 install.sh
+./install.sh
--- views/controller/service/models/db/sql/create_db.sql
+++ views/controller/service/models/db/sql/create_db.sql
@@ -1,10 +1,41 @@
 -- 创建teacher表
 drop table if exists teacher;
 create table teacher(
-    id int not null primary key,
+    teacher_id varchar(20) not null primary key,
     password varchar(200) not null,
     name varchar(200) default null,
     sex varchar(200) default null,
     age smallint default null,
-    college varchar(200) default null
+    college varchar(200) default null,
+    credit smallint default 0,
+    pub_date timestamp null DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 创建 task 表
+drop table if exists task;
+create table task(
+    task_id int not null primary key AUTO_INCREMENT,
+    teacher_id varchar(20) default null,
+    finance_id varchar(20) default null,
+    reservate_time timestamp null,
+    actual_time timestamp null,
+    status varchar(200) default '待完成'
+);
+
+-- 创建财务人员表
+drop table if exists finance;
+create table finance(
+    finance_id varchar(20) not null primary key,
+    password varchar(200) not null,
+    name varchar(200) default null,
+    sex varchar(200) default null,
+    age smallint default null
+);
+
+-- 创建窗口表
+drop table if exists window;
+create table window(
+    window_id varchar(20) not null primary key,
+    finance_id varchar(20) null,
+    status varchar(200) null
 );
