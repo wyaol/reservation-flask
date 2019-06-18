9779f1fd71cb0ef288747df4f93eb795e6d28ead
wyao <289672494@qq.com>
2019-06-18 14:37:08 +0800

    修改sql bug

--- views/controller/service/models/db/sql/create_db.sql
+++ views/controller/service/models/db/sql/create_db.sql
@@ -1,9 +1,10 @@
-// 创建teacher表
+-- 创建teacher表
+drop database if exists teacher;
 create table teacher(
     id int not null primary key,
     password varchar(200) not null,
     name varchar(200) default null,
     sex varchar(200) default null,
     age smallint default null,
-    college varchar(200) default null,
+    college varchar(200) default null
 );
