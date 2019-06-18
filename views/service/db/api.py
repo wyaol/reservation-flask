138ff0308491bfe4009c4f5ed4d8f339160bfb21
wyao <289672494@qq.com>
2019-06-18 21:01:28 +0800

    增加 删除 更新 增加 功能

--- views/service/db/api.py
+++ views/service/db/api.py
@@ -16,10 +16,20 @@ class SQL:
         # 得到一个可以执行SQL语句的光标对象
         self.cursor = self.conn.cursor()
 
-    def insert(self, table_name, *argvs, **kwargs):
+    def insert(self, table_name,**kwargs):
         sql = 'insert into %s (%s) values (%s)'%(table_name,
                                                  SQL.keys2str(list(kwargs.keys())),
                                                  SQL.values2str(list(kwargs.values())))
+        self.cursor.execute(sql)
+        self.conn.commit()
+
+    def delete(self, table_name, **kwargs):
+        sql = 'delete from %s where %s'%(table_name, SQL.dict2str(kwargs))
+        self.cursor.execute(sql)
+        self.conn.commit()
+
+    def update(self, table_name: str, set: dict, where: dict):
+        sql = 'update %s set %s where %s'%(table_name, SQL.dict2str(set, ', '), SQL.dict2str(where))
         self.cursor.execute(sql)
         self.conn.commit()
 
@@ -30,9 +40,34 @@ class SQL:
     @staticmethod
     def values2str(keys: list):
         for i in range(len(keys)):
-            if isinstance(keys[i], str):
-                keys[i] = "'%s'" % keys[i]
+            keys[i] = SQL.add_quotes(keys[i])
         return ', '.join(keys)
+
+    @staticmethod
+    def dict2str(kwargs: dict, connector: str=' and '):
+        """
+        格式化字典为字符串
+        :param kwargs: 传入的字典
+        :param connector: 最终连接符
+        :return:
+        """
+        items = kwargs.items()
+        items_list = []
+        for item in items:
+            item = list(item)
+            item[1] = SQL.add_quotes(item[1])
+            items_list.append('='.join(item))
+        return connector.join(items_list)
+
+    @staticmethod
+    def add_quotes(field):
+        """
+        给str类型字段加上单引号
+        :param field: 字段 未知类型
+        :return:
+        """
+        if isinstance(field, str):
+            return "'%s'"%field
 
     def close(self):
         """
