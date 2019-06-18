32675a19f18aa179b1128e5a217f9344833eec63
wyao <289672494@qq.com>
2019-06-19 00:07:14 +0800

    修改session

--- views/controller/service/session.py
+++ views/controller/service/session.py
@@ -7,20 +7,9 @@ login_manager = LoginManager()
 
 @login_manager.user_loader
 def load_user(user_id):
-    session = Session.query_session(user_id)
-    if session is not None:
-        return session
+    return Session(user_id)
 
 class Session(UserMixin):
 
-    sessions = []
-
     def __init__(self, id):
         self.id = id
-        Session.sessions.append(self)
-
-    @classmethod
-    def query_session(cls, id):
-        for e in cls.sessions:
-            if e.id == id:
-                return e
