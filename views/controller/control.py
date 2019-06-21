62859f1afbba2b28b4aad78c0bbb1f22a2e1b8a5
wyao <289672494@qq.com>
2019-06-21 21:31:10 +0800

    修改bug

--- views/controller/control.py
+++ views/controller/control.py
@@ -7,9 +7,10 @@ from .controller_exception import IdentityNotExistException
 def login(code):
     open_id = main_service.get_open_id(code)
     teacher_id = teacher_service.get_teacher_id(open_id)
+    print(teacher_id)
     session['open_id'] = open_id
     if teacher_id is not None:
-        teacher_service.login(teacher_id=teacher_id)
+        session['id'] = teacher_id
         return True
     return False
 
