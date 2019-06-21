31669132c03be7d60912fe27fd44826c2d8d6653
wyao <289672494@qq.com>
2019-06-21 21:08:11 +0800

    修改bug

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
 from views.identity_control_views import identity_control_views
 from views.reservate_views import reservate_views
@@ -7,7 +8,7 @@ import config
 
 app = Flask(__name__)
 app.register_blueprint(identity_control_views, url_prefix='/api/identity')
-app.register_blueprint(identity_control_views, url_prefix='/api/reservate')
+app.register_blueprint(reservate_views, url_prefix='/api/reservate')
 app.secret_key = '123456'
 app.permanent_session_lifetime = timedelta(days=1) #设置session的保存时间。
 
